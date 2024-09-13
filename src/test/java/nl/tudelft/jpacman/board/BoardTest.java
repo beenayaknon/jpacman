ac29631b7b88ab65aed8c2b1d5c9a15b432d8c06
Beenayaknon <su.beepak@gmail.com>
2024-09-14 00:10:42 +0700

    Add BoardTest

---
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -0,0 +1,43 @@
+package nl.tudelft.jpacman.board;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class BoardTest {
+    private Board board;
+    private Square[][] grid;
+    private Board boardNull;
+    private Square[][] gridNull;
+    @BeforeEach
+    public void setUp() {
+        grid = new Square[1][1];
+        grid[0][0] = new BasicSquare();
+        board = new Board(grid);
+
+//        Create null grid
+        gridNull = new Square[1][1];
+        gridNull[0][0] = null;
+        boardNull = new Board(gridNull);
+    }
+
+    @Test
+    public void testGetWidth() {
+        assertThat(board.getWidth()).isEqualTo(1);
+    }
+
+    @Test
+    public void testGetHeight() {
+        assertThat(board.getHeight()).isEqualTo(1);
+    }
+    @Test
+    public void testSquareAt() {
+        Square square = board.squareAt(0, 0);
+        assertThat(square).isNotNull();
+    }
+
+    @Test
+    public void testSquareAtNull() {
+        Square square = boardNull.squareAt(0, 0);
+        assertThat(square).isNull();
+    }
+}
