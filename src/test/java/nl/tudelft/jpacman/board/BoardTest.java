e86cc37bf16c30d772f88bab64e0fa0dbd23d121
Sushawapak Bee <106221231+beenayaknon@users.noreply.github.com>
2024-11-05 12:01:12 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -4,7 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 public class BoardTest {
-    // Initialize test.
+    /* Initialize test.
+    */
     private Board board;
     private Square[][] grid;
     private Board boardNull;
@@ -15,32 +16,37 @@ public class BoardTest {
         grid[0][0] = new BasicSquare();
         board = new Board(grid);
 
-//        Create null grid.
+/*        Create null grid.
+    */
         gridNull = new Square[1][1];
         gridNull[0][0] = null;
         boardNull = new Board(gridNull);
     }
 
-    // Test testGetWidth.
+    /* Test testGetWidth.
+    */
     @Test
     public void testGetWidth() {
         assertThat(board.getWidth()).isEqualTo(1);
     }
     
- // Test testGetHeight.
+ /* Test testGetHeight.
+    */
     @Test
     public void testGetHeight() {
         assertThat(board.getHeight()).isEqualTo(1);
     }
 
-    // Test testSquareAt.
+    /* Test testSquareAt.
+    */
     @Test
     public void testSquareAt() {
         Square square = board.squareAt(0, 0);
         assertThat(square).isNotNull();
     }
 
-    // Test testSquareAtNull.
+    /* Test testSquareAtNull.
+    */
     @Test
     public void testSquareAtNull() {
         Square square = boardNull.squareAt(0, 0);
